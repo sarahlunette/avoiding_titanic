6e8cff463006c1d253d4937ab045968dbde64852
Sarah Le Net <sarahlunette@users.noreply.github.com>
2024-01-11 11:42:39 +0100

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -104,24 +104,26 @@ if page == pages[0]:
     st.header('Second Option')
     st.write('Icebreaker')
 
-uploaded_files = st.file_uploader(
-    "Choose a PDF file",
-    accept_multiple_files=True,
-    type=['pdf']
-)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-
-    st.write("filename:", uploaded_file.name)
-    base64_pdf = base64.b64encode(bytes_data).decode('utf-8')
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    uploaded_files = st.file_uploader(
+        "Choose a PDF file",
+        accept_multiple_files=True,
+        type=['pdf']
+    )
     
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+    
+        st.write("filename:", uploaded_file.name)
+        base64_pdf = base64.b64encode(bytes_data).decode('utf-8')
+        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+        
+        uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+        
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+        st.write(bytes_data)
 
 if page == pages[1]:
     st.title('Copernicus Data')
